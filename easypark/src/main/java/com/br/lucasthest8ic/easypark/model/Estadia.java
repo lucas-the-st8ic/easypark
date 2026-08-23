409a3d7e0123e8acad0ff6d5de2b9c5d8d0d99c9
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-23 16:40:28 -0300

    Updates

---
+++ easypark/src/main/java/com/br/lucasthest8ic/easypark/model/Estadia.java
@@ -0,0 +1,11 @@
+package com.br.lucasthest8ic.easypark.model;
+
+import java.time.LocalDateTime;
+
+public class Estadia {
+    private Vaga vaga;
+    private Carro carro;
+    private Double valor;
+    private LocalDateTime horarioEntrada;
+    private LocalDateTime horarioSaida;
+}
--- easypark/src/main/java/com/br/lucasthest8ic/easypark/model/Vaga.java
+++ easypark/src/main/java/com/br/lucasthest8ic/easypark/model/Vaga.java
@@ -1,8 +1,8 @@
 package com.br.lucasthest8ic.easypark.model;
 
 public class Vaga {
+    private Estacionamento estacionamento;
     private int idVaga;
     private String tipo;
-    private boolean vagaLivre;
-    private Carro carro;
+    
 }
