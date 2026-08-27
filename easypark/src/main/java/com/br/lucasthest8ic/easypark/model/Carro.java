bbc02199c2b86e0ba4fa8cf8b61f439928159a94
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-27 13:33:51 -0300

    Update Carro.java

--- easypark/src/main/java/com/br/lucasthest8ic/easypark/model/Carro.java
+++ easypark/src/main/java/com/br/lucasthest8ic/easypark/model/Carro.java
@@ -1,20 +1,45 @@
 package com.br.lucasthest8ic.easypark.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="carros")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Carro {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(
+            name = "carro_seq_generator",      // Nome lógico interno no JPA/Hibernate
+            sequenceName = "seq_carros_id",    // Nome da SEQUENCE criada no PostgreSQL
+            initialValue = 1,                  // Começa em 1
+            allocationSize = 1                 // Incrementa de 1 em 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "carro_seq_generator"  // Aponta exatamente para o name acima
+    )
     private Integer idCarro;
 
-
-    private String modelo;
-    @Column(unique = true)
+    @Column(name = "placa",
+            nullable = false,
+            unique = true, length = 10)
     private String placa;
+
+    @Column(name = "modelo", length = 50)
+    private String modelo;
+
+    @Column(name = "cor", length = 30)
     private String cor;
-    private boolean elegivelVagaIdoso;
+
+    @Column(name = "elegivel_vaga_idoso",
+            nullable = false)
+    private boolean elegivelVagaIdoso = false;
 
 }
