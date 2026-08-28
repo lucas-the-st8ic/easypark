7b816e92b4914696dd746faddee0558cf5861919
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-28 10:07:21 -0300

    Update Estacionamento.java

--- easypark/src/main/java/com/br/lucasthest8ic/easypark/model/Estacionamento.java
+++ easypark/src/main/java/com/br/lucasthest8ic/easypark/model/Estacionamento.java
@@ -1,9 +1,14 @@
 package com.br.lucasthest8ic.easypark.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.util.List;
 
+@Entity
 public class Estacionamento {
 
+    @Id
     private Integer idEstacionamento;
 
 
